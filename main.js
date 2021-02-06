f969af08b1c8b1288ed6db2d5a5fd79fc8e52afc
Roydrigo <lrmagallanj513@gmail.com>
2021-02-05 22:22:34 -0600

    crPlaceholder en input y funcion selector

--- main.js
+++ main.js
@@ -10,9 +10,21 @@ header.innerHTML = 'ToDo APP'
 document.body.appendChild(header)
 
 input.type = 'input'
-input.innerText='¿Que tarea quieres agregar?'
+input.setAttribute("placeholder", "Escribe aqui tu tarea")
 document.body.appendChild(input)
+
 
 button.type = 'button'
 button.innerText='Añadir Tarea'
 document.body.appendChild(button)
+
+
+$("button").addEventListener("click", function(){ //Listener al evento del botón
+    let input = document.createElement('input') 
+
+    console.log('input')
+})
+
+function $(selector){
+    return document.querySelector(selector); //Selececionar tupo Jquery
+}
