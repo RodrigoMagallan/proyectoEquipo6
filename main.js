d23dc5d394fc270c3e526e3942f1a0419fbda28a
Roydrigo <lrmagallanj513@gmail.com>
2021-02-05 22:37:06 -0600

    B.

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const header = document.createElement('h2')
 
 console.log(app)
 
+//Creación de header h2
 header.type = 'h2'
 header.innerHTML = 'ToDo APP'
 document.body.appendChild(header)
 
+//Creación de input con placeholde
 input.type = 'input'
 input.setAttribute("placeholder", "Escribe aqui tu tarea")
 document.body.appendChild(input)
 
-
+//Creación de botón
 button.type = 'button'
 button.innerText='Añadir Tarea'
 document.body.appendChild(button)
