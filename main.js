d0a87b35886565c9212f53620bc06da9f3b80040
Janz Cázares <fermix_halo332@hotmail.com>
2021-02-05 22:25:39 -0600

    Script para generar la tabla

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-const formulario = document.getElementById('formulario')
-const input = document.getElementById('input')
-const listaTareas = document.getElementById('listaTareas')
-const template = document.getElementById('template').content
-//agrupar fragment porque son volatiles 
-const fragment = document.createDocumentFragment()
-let tareas = { //colección de objectos, para hacer mas rapido el poder obtener un elemento.
-    1 : {
-        id : 1,
-        text: 'Tarea #1',
-        estado: false
-    }, 
-    2 :{
-        id : 2,
-        text : 'Tarea #2',
-        estado: false
-    }
+var row = document.createElement("tr");
+
+
+function buildTableTasks(){
+    var table = document.createElement("table");
+    var tableBody = document.createElement("tbody");
+    table.appendChild(tableBody);
+    document.getElementById("listTask")
+    .appendChild(table);
 }
 
-formulario.addEventListener('submit', e => {
-    e.preventDefault()
-    //console.log(e.target[0].value)
-    setTarea(e)
-})
+window.onload =  buildTableTasks();
 
-const setTarea = e => {
-    if(input.value.trim() === ''){
-        console.log('esta vacío')
-        return
-    }
-    console.log('diste clic')
 
-    formulario.reset()
-}
+
+//Variables globales
+var btnSave = document.getElementById("btnSave");
+var task = "";
+var tasksArr = [];
+
+
+//Función del botón guardar
+btnSave.addEventListener('click',function(event){
+    task = { tasks: document.getElementById('inputTask').value};
+    tasksArr.push(task);
+    console.log(tasksArr);
+    document.getElementById('inputTask').value = "";
+});
+
+
+
