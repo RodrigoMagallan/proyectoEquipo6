f8aa11fb4fcbad7c3161d4783fcd336beb8f5a8c
Janz Cázares <fermix_halo332@hotmail.com>
2021-02-07 19:19:20 -0600

    Estilos, y conjuntos js

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 var app = document.getElementById("app");
 const input = document.createElement('input');
 const button = document.createElement('button');
-const header = document.createElement('h2');
+const header = document.createElement('h1');
+
 
 //Creación de header h2
-header.type = 'h2';
+header.type = 'h1';
 header.innerHTML = 'ToDo APP';
+header.setAttribute('class', 'title');
 app.appendChild(header);
 
 //Creación de input con placeholde
 input.type = 'input';
 input.setAttribute("placeholder", "Writte a task here");
-input.setAttribute('id','inputTask');
+input.setAttribute('id', 'inputTask');
 app.appendChild(input);
 
 //Creación de botón
 button.type = 'button';
-button.innerText='Add a task';
-button.setAttribute('id','btnSave');
+button.innerText = 'Add a task';
+button.setAttribute('id', 'btnSave');
 app.appendChild(button);
 
 //Crea el elemento tbody
 var tableBody = document.createElement("tbody");
 
 //Construye la tabla
-function buildTableTasks(){
+function buildTableTasks() {
     var table = document.createElement("table");
-    table.setAttribute('id','tableTask');
+    table.setAttribute('id', 'tableTask');
     table.appendChild(tableBody);
     document.getElementById("app")
-    .appendChild(table);
+        .appendChild(table);
 }
 
 //Setea la tabla al cargar la página
-window.onload =  buildTableTasks();
+window.onload = buildTableTasks();
 
 //Variables globaless 
 var btnSave = document.getElementById("btnSave");
@@ -42,36 +44,60 @@ var tasksArr = [];
 var btnDelete = document.getElementById("btnDelete");
 
 
-//Función del botón guardar
-btnSave.addEventListener('click',function(event){
+//Función del botón guardar, genera los campos, botones y checks
+btnSave.addEventListener('click', function (event) {
 
-if(document.getElementById('inputTask').value == ""){
-    window.alert("No se puede agregar un registro vacio");
-}else{
-    task =  document.getElementById('inputTask').value;
-    tasksArr.push(task);
-    console.log(tasksArr);
-    var row = document.createElement("tr");
-    var col = document.createElement("td");
-    var btnDelete = document.createElement("button");
-    btnDelete.type = 'button';
-    btnDelete.setAttribute('id','btnDelete');
-    btnDelete.innerText= "Delete";
-    var checkTask = document.createElement("input");
-    checkTask.type = "checkbox";
-        col.appendChild(checkTask);
-        col.appendChild(document.createTextNode(task));
-        col.appendChild(btnDelete);
-        row.appendChild(col);
-    tableBody.appendChild(row);
-    document.getElementById('inputTask').value = "";
-}
-//Funcionalidad del botón de eliminar
-btnDelete.addEventListener('click',function (event) {
-    var i = this.parentNode.parentNode.rowIndex; //Elimina la fila en la que se de click
-    document.getElementById('tableTask').deleteRow(i);
- })
- 
+    if (document.getElementById('inputTask').value == "") {
+        window.alert("No se puede agregar un registro vacio");
+    } else {
+        task = document.getElementById('inputTask').value;
+        tasksArr.push(task);
+        var row = document.createElement("tr");
+        var col = document.createElement("td");
+        var colCheck = document.createElement("td");
+        var colBtn = document.createElement("td");
+        var btnDelete = document.createElement("button");
+        var checkTask = document.createElement("input");
+        var pe = document.createElement('p');
+        pe.setAttribute('id', 'text');
+        checkTask.setAttribute('class', 'check');
+        checkTask.setAttribute('id', 'checkLi');
+        btnDelete.type = 'button';
+        btnDelete.setAttribute('id', 'btnDelete');
+        btnDelete.innerText = "Delete";
+        checkTask.type = "checkbox";
+        colCheck.appendChild(checkTask);
+        row.appendChild(colCheck);
+        pe.appendChild(document.createTextNode(task));
+        row.appendChild(pe);
+        colBtn.appendChild(btnDelete);
+        row.appendChild(colBtn);
+        tableBody.appendChild(row);
+        document.getElementById('inputTask').value = "";
+    }
+    //Funcionalidad del botón de eliminar
+    btnDelete.addEventListener('click', function (event) {
+        var i = this.parentNode.parentNode.rowIndex; //Elimina la fila en la que se de click
+        document.getElementById('tableTask').deleteRow(i);
+    })
+
+
+    //Agregar linea de marcado
+    
+    checkTask.addEventListener('change', function () {
+        var i = this.parentNode.parentNode.rowIndex;
+        if (this.checked) {
+            pe.setAttribute('class', 'checkList');
+        } else {
+            pe.removeAttribute('class', 'checkList');
+        }
+    });
+
 });
+
+
+
+
+
 
 
